6aba0397f5503539168642703fd34b1a35db097a
Ali Haidar <alighaidar97@gmail.com>
2022-07-10 21:08:52 +0300

    fetch employees

--- src/pages/employees/employee.js
+++ src/pages/employees/employee.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import EmployeeCard from "../../components/employee-card/employeeCard";
 
 const Employees = () => {
+
+  const [data, setData] = useState([]);
+
+  const fetchEmployees = async () => {
+    try {
+      const response = await fetch('http://localhost:8000/api/employees');
+      const res = await response.json();
+      console.table(res);
+      setData(res);
+    }
+    catch {
+      console.log('error');
+    }
+  }
+
+  useEffect(() => {
+    fetchEmployees();
+  }, []);
+
+
   return (
-    <div>
-      <h1>Employees</h1>
-    </div>
-  );
+    <>
+      <h1>employees</h1>
+      { data && 
+        <EmployeeCard data={data} />
+      }
+    </>
+  )
 };
 export default Employees;
