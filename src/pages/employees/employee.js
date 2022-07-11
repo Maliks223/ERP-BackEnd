8435f87ea9d89982ee9e20aa170f7e0d97d1ad17
Ali Haidar <alighaidar97@gmail.com>
2022-07-11 12:22:27 +0300

    employees table

--- src/pages/employees/employee.js
+++ src/pages/employees/employee.js
@@ -1,6 +1,33 @@
 import React, { useEffect, useState } from "react";
-import EmployeeCard from "../../components/employee-card/employeeCard";
 import "./employee.css";
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
 
 
 const Employees = () => {
@@ -27,9 +54,33 @@ const Employees = () => {
   return (
     <>
       <h1>employees</h1>
-      { data && 
-        <EmployeeCard data={data} />
-      }
+      <TableContainer component={Paper}>
+      <Table  aria-label="customized table">
+      {/* sx={{ Width: 500}} */}
+        <TableHead>
+          <TableRow>
+            <StyledTableCell align="center">Profile Picture</StyledTableCell>
+            <StyledTableCell align="center">First Name</StyledTableCell>
+            <StyledTableCell align="center">Last Name</StyledTableCell>
+            <StyledTableCell align="center">Email</StyledTableCell>
+            <StyledTableCell align="center">Team</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((employee) => (
+            <StyledTableRow key={employee.id}>
+              <StyledTableCell component="th" scope="row">
+                {data.id}
+              </StyledTableCell>
+              <StyledTableCell align="center">{employee.firstname}</StyledTableCell>
+              <StyledTableCell align="center">{employee.lastname}</StyledTableCell>
+              <StyledTableCell align="center">{employee.email}</StyledTableCell>
+              <StyledTableCell align="center">{employee.id}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
     </>
   )
 };
