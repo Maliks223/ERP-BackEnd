22bfc7c83be9f9ebe1e9061e697babcfc3b6f11b
malik <malikakkawi223@gmail.com>
2022-07-14 13:04:52 +0300

    setting design & functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import './App.css';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import Home from './pages/Home/home';
-import Employees from './pages/employees/employee';
-import Teams from './pages/teams/team';
-import Projects from './pages/projects/project';
-import SideBar from './components/sidebar/sidebar';
-import KPIS from './pages/kpi/kpi';
+import "./App.css";
+import {
+  Route,
+  BrowserRouter as Router,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Home from "./pages/Home/home";
+import Employees from "./pages/employees/employee";
+import Teams from "./pages/teams/team";
+import Projects from "./pages/projects/project";
+// import SideBar from './components/sidebar/sidebar';
+import KPIS from "./pages/kpi/kpi";
+import Login from "./pages/login/login";
+import Dashboard from "./pages/Dashboard";
 function App() {
-
   return (
     <>
       <Router>
-        <SideBar />
         <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/employees" element={<Employees />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/kpis" element={<KPIS />} />
-          <Route path="/teams" element={<Teams />} />
+          <Route path="*" element={<Navigate to="/home" replace />} />
+          <Route path="login" element={<Login />} />
+          <Route path="/" element={<Dashboard />}>
+            <Route path="home" element={<Home />} />
+            <Route path="employees" element={<Employees />} />
+            <Route path="projects" element={<Projects />} />
+            <Route path="kpis" element={<KPIS />} />
+            <Route path="teams" element={<Teams />} />
+          </Route>
         </Routes>
       </Router>
     </>
