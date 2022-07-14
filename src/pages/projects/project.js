b449001b0659c14a3e3473a59e81ced6fcdffe83
Malek-Tazkarji <malektazkarjii@gmail.com>
2022-07-14 13:06:11 +0300

    projects

--- src/pages/projects/project.js
+++ src/pages/projects/project.js
@@ -2,14 +2,42 @@ import React, { useEffect, useState } from "react";
 import "./project.css";
 import Project from "./projectmodule";
 import Axios from "axios";
-import { Dialog } from "@mui/material";
+import { Dialog, DialogContent, TextField } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
+import DialogTitle from "@mui/material/DialogTitle";
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
+}));
 
 const Projects = () => {
   const [projects, setprojects] = useState([]);
   const [postname, setpostname] = useState("");
   const [post, setPost] = useState(false);
-
-
 
   const Request = async () => {
     const res = await Axios.get("http://localhost:8000/api/project").catch(
@@ -27,77 +55,93 @@ const Projects = () => {
     const data = new FormData();
     data.append("name", postname);
 
-    await Axios
-      .post("http://localhost:8000/api/project", data)
-      .catch((err) => console.log(err));
+    await Axios.post("http://localhost:8000/api/project", data).catch((err) =>
+      console.log(err)
+    );
   };
 
   return (
     <>
-     <button
-            id="icons"
-            onClick={() => {
-              setPost(!post);
-            }}
-          >
-            postttttttttttttttttttttttttttttttttttttttttttttttttttttttt
-            {/* <i id="post" className="singlePostIcon far fa-edit"></i>{" "} */}
-          </button>
-          {post && (
-        <Dialog open={post}onClose={()=>{setPost(!post)}}>
-        <div>
-          <form
-            onSubmit={(e) => {
-              handlePost();
-            }}
-          >
-            <input
-              name="name"
-              placeholder="projectName"
-            //   onFocus={true}
-              type="text"
-              onChange={(e) => {
-                setpostname(e.target.value);
+      <button
+        id="icons"
+        onClick={() => {
+          setPost(!post);
+        }}
+      >
+        postttttttttttttttttttttttttttttttttttttttttttttttttttttttt
+      </button>
+      {post && (
+        <Dialog
+          open={post}
+          onClose={() => {
+            setPost(!post);
+          }}
+        >
+          <DialogTitle style={{ marginBottom: "40px" }}>
+            Create Project
+          </DialogTitle>
+
+          <DialogContent>
+            <form
+              onSubmit={(e) => {
+                handlePost();
               }}
-            />
-            <button type="submit">submit</button>
-          </form>
-        </div>
+            >
+              <TextField
+                name="name"
+                placeholder="projectName"
+                //   onFocus={true}
+                type="text"
+                onChange={(e) => {
+                  setpostname(e.target.value);
+                }}
+              />
+              <Button
+                type="submit"
+                style={{ marginTop: "80px", marginRight: "20px" }}
+              >
+                submit
+              </Button>
+            </form>
+          </DialogContent>
         </Dialog>
       )}
-
-    <div className="ProjectContainer">
-      {projects.map((project, index) => {
-        return (
-          <Project
-            key={index}
-            name={project.name}
-            id={project.id}
-            // teamz={project.team[0].name}
-            //  team={project.team[1]}
-            teamz={project.team.map((teams, index) => {
+      <TableContainer component={Paper}>
+        <Table
+          sx={{ marginLeft: "15vw", width: "80vw" }}
+          aria-label="customized table"
+        >
+          <TableHead>
+            <TableRow>
+              <StyledTableCell align="center">id</StyledTableCell>
+              <StyledTableCell align="center">Project name</StyledTableCell>
+              <StyledTableCell align="center">Team</StyledTableCell>
+              <StyledTableCell align="center">Actions</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {projects.map((project, index) => {
               return (
-                <div key={index}>
-                  {teams.name}
-                 
-
-                   
-                  
-                </div>
+                <Project
+                  key={index}
+                  name={project.name}
+                  id={project.id}
+                  // teamz={project.team[0].name}
+                  //  team={project.team[1]}
+                  teamz={project.team.map((teams, index) => {
+                    return <div key={index}>{teams.name}</div>;
+                  })}
+                />
               );
             })}
-          />
-        );
-      })}
-    </div>
-  </>);
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+ 
+    </>
+  );
 };
 export default Projects;
 
 
-
- // .map((fuckingteam,index)=>{
-                    // return (<div key={index}>
-                    // {fuckingteam.name}
-                    //     </div>)
-                    // })
--- src/pages/projects/projectmodule.js
+++ src/pages/projects/projectmodule.js
@@ -1,50 +1,46 @@
-import React, { useState ,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-// import { Dialog } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableRow from "@mui/material/TableRow";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 import Dialog from "@mui/material/Dialog";
-import { styled } from '@mui/material/styles';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
 
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
-    [`&.${tableCellClasses.head}`]: {
-      backgroundColor: theme.palette.common.black,
-      color: theme.palette.common.white,
-    },
-    [`&.${tableCellClasses.body}`]: {
-      fontSize: 14,
-    },
-  }));
-  
-  const StyledTableRow = styled(TableRow)(({ theme }) => ({
-    '&:nth-of-type(odd)': {
-      backgroundColor: theme.palette.action.hover,
-    },
-    // hide last border
-    '&:last-child td, &:last-child th': {
-      border: 0,
-    },
-  }));
-  
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
 
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
+}));
 
-
-
-const Project = ({ id, name,teamz}) => {
+const Project = ({ id, name, teamz }) => {
   const [Name, setName] = useState(name);
   const [edit, setEdit] = useState(false);
   const [assignteam, setassignteam] = useState(false);
-  const [team,setteam]=useState([]);
-
-  const [teamzz,setteamzz]=useState("");
-  const [project,setproject]=useState(name);
-
+  const [opendelete, handleclosedelete] = useState(false);
+  const [team, setteam] = useState([]);
+  const [teamzz, setteamzz] = useState("");
+  const [project, setproject] = useState(name);
 
   const handleSubmit = async (id) => {
     const data = new FormData();
@@ -57,7 +53,6 @@ const Project = ({ id, name,teamz}) => {
   };
 
   const handleDelete = async (id) => {
-
     await axios
       .delete(`http://localhost:8000/api/project/${id}`)
       .then((response) => response.data)
@@ -65,70 +60,100 @@ const Project = ({ id, name,teamz}) => {
       .catch((err) => console.log(err));
   };
 
- 
+  const Projectteam = async () => {
+    const data = new FormData();
+    data.append("Team_id", teamzz);
+    data.append("Project_id", project);
 
-const Projectteam=async()=>{
-const data=new FormData();
-data.append("Team_id",teamzz);
-data.append("Project_id",project);
+    await axios
+      .post("http://localhost:8000/api/teamproject", data)
+      .catch((err) => console.log(err));
+  };
 
-  await axios.post("http://localhost:8000/api/teamproject",data)
-    .catch((err)=>console.log(err));
-  }
-
-const func=(e)=>{
-  setteamzz(e.target.value)
-}
+  const func = (e) => {
+    setteamzz(e.target.value);
+  };
   const Request = async () => {
-    const res = await axios.get("http://localhost:8000/api/teams").catch(
-      (err) => console.log(err)
-    );
+    const res = await axios
+      .get("http://localhost:8000/api/teams")
+      .catch((err) => console.log(err));
     const data = await res.data;
     console.log(data);
-     setteam(data);
+    setteam(data);
   };
   useEffect(() => {
     Request();
   }, []);
 
-
-
-  
-
-
-
- 
-
   return (
     <>
-    <TableContainer component={Paper}>
-    <Table  aria-label="customized table">
-    <TableHead>
-          <TableRow>
-            <StyledTableCell align="center">Project name</StyledTableCell>
-            <StyledTableCell align="center">teams</StyledTableCell>
-            <StyledTableCell align="center">id</StyledTableCell>
+      <StyledTableRow key={id}>
+        <StyledTableCell component="th" scope="row" align="center">
+          {id}
+        </StyledTableCell>
+        <StyledTableCell align="center">{name}</StyledTableCell>
+        <StyledTableCell align="center">
+          {teamz ? teamz : "<h1>No Teams Assigned<h1>"}
+        </StyledTableCell>
+        <StyledTableCell align="center">
+          <Button
+            onClick={() => {
+              setEdit(!edit);
+            }}
+          >
+            <EditIcon color="success" />
+          </Button>
+          <Button
+            onClick={(e) => {
+              handleclosedelete(!opendelete);
+            }}
+          >
+            <DeleteIcon color="error" />
+          </Button>
+          <Button
+            style={{
+              color: "blue",
+              border: "0.1px solid black",
+              borderRadius: "2px",
+            }}
+            id="icons"
+            onClick={() => {
+              setassignteam(!assignteam);
+            }}
+          >
+            assign to a team
+          </Button>
+        </StyledTableCell>
+        <Dialog
+          open={opendelete}
+          onClose={(e) => {
+            handleclosedelete(!opendelete);
+          }}
+        >
+          <DialogTitle>Delete</DialogTitle>
+          <DialogContent>
+            Are you sure you want to Delete this Employee?
+          </DialogContent>
+          <DialogActions>
+            <Button
+              onClick={(e) => {
+                handleDelete(id);
+              }}
+              color="error"
+            >
+              Yes
+            </Button>
+            <Button
+              onClick={(e) => {
+                handleclosedelete(!opendelete);
+              }}
+            >
+              No
+            </Button>
+          </DialogActions>
+        </Dialog>
 
-          </TableRow>
-          </TableHead>
-          <TableBody>
-            <StyledTableRow>
-              <StyledTableCell component="th" scope="row">
-              
-              </StyledTableCell>
-              <StyledTableCell align="center">{name}</StyledTableCell>
-              <StyledTableCell align="center">{teamz}</StyledTableCell>
-              <StyledTableCell align="center">{id}</StyledTableCell>
-
-      
-            </StyledTableRow>
-        </TableBody>
-      <div>
-        <h1>name :{name}</h1>
-        <h1>name :{teamz}</h1>
-
-        <div>
-          <button
+        {/* <button
             sytle={{ padding: "10px" }}
             id="icon"
             onClick={(e) => {
@@ -136,7 +161,6 @@ const func=(e)=>{
             }}
           >
             delete
-            {/* <i id="delete" className="singlePostIcon far fa-trash-alt"></i> */}
           </button>
           <button
             id="icons"
@@ -145,28 +169,15 @@ const func=(e)=>{
             }}
           >
             edit
-            {/* <i id="editt" className="singlePostIcon far fa-edit"></i>{" "} */}
           </button>
-         
-          <button
-            id="icons"
-            onClick={() => {
-              setassignteam(!assignteam);
-            }}
-          >
-            assign to a team
-            {/* <i id="team" className="singlePostIcon far fa-edit"></i>{" "} */}
-          </button>
-        </div>
-      </div>
+          */}
 
-      <div>
         {edit && (
           <Dialog open={edit} onClose={() => setEdit(!edit)}>
-          <div>
-            <form onSubmit={(e) => handleSubmit(id)}>
-              <div>
-                <input
+            <DialogTitle>Edit</DialogTitle>
+            <DialogContent>
+              <form onSubmit={(e) => handleSubmit(id)}>
+                <TextField
                   defaultValue={name}
                   name="name"
                   type="text"
@@ -176,41 +187,63 @@ const func=(e)=>{
                     setName(e.target.value);
                   }}
                 />
-                <button className="submitbutton" type="submit">
-                  submit
-                </button>
-              </div>
-            </form>
-          </div>
+                <DialogActions>
+                  <Button className="submitButton" type="submit">
+                    Edit
+                  </Button>
+                </DialogActions>
+              </form>
+            </DialogContent>
           </Dialog>
         )}
-      </div>
 
-      
-      {assignteam &&
-<Dialog open={assignteam}onClose={()=>setassignteam(!assignteam)}>
-      <div>
-        <form onSubmit={(e)=>{Projectteam()}}>
-          {/* <input defaultValue={project} name="Project_id"/>
-            <label htmlFor="team">Choose a team:</label>
-             <div  > 
-          
-          </div>  */}
-<input name="project"type="text"placeholder="project" onChange={(e)=>{setproject(e.target.value)}}/>
-<div>
-<select  name="team"id="team"onChange={func}>
-            {team && team.map((e, i) => <option key={i} value={e.id}>{e.name}</option>)}
-          </select>
-          </div>
-          <button type="submit">submit</button>
+        {assignteam && (
+          <Dialog open={assignteam} onClose={() => setassignteam(!assignteam)}>
+            <DialogTitle>Assign Project to a Team</DialogTitle>
 
-        </form>
-      </div>
-      
-      </Dialog>
-      }
-      </Table>
-      </TableContainer>
+            <DialogContent>
+              <form
+                onSubmit={(e) => {
+                  Projectteam();
+                }}
+              >
+                {/* <TextField defaultValue={project} name="Project_id"/> */}
+                {/* <label htmlFor="team">Choose a team:</label> */}
+
+                <TextField
+                  name="project"
+                  type="text"
+                  placeholder="project"
+                  label="project id"
+                  onChange={(e) => {
+                    setproject(e.target.value);
+                  }}
+                />
+                <select
+                  name="team"
+                  id="team"
+                  onChange={func}
+                  style={{ padding: "3px 3px 10px 3px", margin: "11px" }}
+                >
+                  <option>Teams</option>
+                  {team &&
+                    team.map((e, i) => (
+                      <option key={i} value={e.id}>
+                        {e.name}
+                      </option>
+                    ))}
+                </select>
+
+                <DialogActions>
+                  <Button className="submitButton" type="submit">
+                    Submit
+                  </Button>
+                </DialogActions>
+              </form>
+            </DialogContent>
+          </Dialog>
+        )}
+      </StyledTableRow>
     </>
   );
 };
