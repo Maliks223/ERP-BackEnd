c6f24e84280e5be81e5d18a4a9156f6e7f1202a4
Ali Haidar <alighaidar97@gmail.com>
2022-07-19 00:21:17 +0300

    fetch employee in profile

--- src/pages/Employee profile/employeeProfile.js
+++ src/pages/Employee profile/employeeProfile.js
@@ -1,12 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Phone } from "@mui/icons-material";
 import { Button, Dialog, DialogContent, DialogTitle } from "@mui/material";
 import { useLocation } from "react-router-dom";
 import './employeeProfile.css';
 import KPIAssignForm from "../../components/KPI_assign/KPI_assign_form";
+import EditEmployeeTeam from "../../components/Edit_Employee_team/editEmployeeTeam";
 
 const EmployeeProfile = () => {
     const [open, setOpen] = useState(false);
+    const [openTeam, setOpenTeam] = useState(false);
+
     const handleClickOpen = () => {
         setOpen(true);
     };
@@ -15,36 +18,64 @@ const EmployeeProfile = () => {
         setOpen(false);
     };
 
+    const handleClickOpenTeam = () => {
+        setOpenTeam(true);
+    };
+
+    const handleCloseTeam = () => {
+        setOpenTeam(false);
+    };
+
     const location = useLocation();
     const data = location.state?.data;
-    const { id, firstname, lastname, email, phonenumber, teams, image, kpis } = data;
 
-    const assignKPI = (e) => {
-        e.preventDefault();
+    const [employee, setEmployee] = useState([]);
+
+    const fetchEmployee = async () => {
+        const response = await fetch(`http://localhost:8000/api/employees/${data.id}`);
+        console.log(response);
+        const res = await response.json();
+        console.log(res);
+        setEmployee(res[0]);
     }
+
+    useEffect(() => {
+        fetchEmployee();
+    }, []);
 
     return (
         <>
-            <div className="employee-profile">
-                <img className='profile-image' src={`http://localhost:8000/storage/uploads/${image}`} />
-                <h3>{firstname} {`  `}{lastname}</h3>
-                <h3>Email: {email}</h3>
-                <h3>Phone Number: {phonenumber}</h3>
-                <h3>Team: {teams.name}</h3>
-                <h3>KPIS: {kpis.map(kpi => {
-                    return (
-                        <p>KPI name: {kpi.name} rate: {kpi.pivot.rate} date: {kpi.pivot.KPI_date}</p>
-                    )
-                })}</h3>
-                <Button onClick={handleClickOpen} >assign KPI</Button>
-                {/* onClick={assignKPI} */}
-                <Dialog open={open} onClose={handleClose}>
-                    <DialogTitle> Assign a KPI</DialogTitle>
-                    <DialogContent>
-                        <KPIAssignForm />
-                    </DialogContent>
-                </Dialog>
-            </div>
+            {employee &&
+                <div className="employee-profile">
+
+                    <img className='profile-image' src={`http://localhost:8000/storage/uploads/${employee.image}`} />
+                    <h3>{employee.firstname} {`  `}{employee.lastname}</h3>
+                    <h3>Email: {employee.email}</h3>
+                    <h3>Phone Number: {employee.phonenumber}</h3>
+                    <h3>Team: {employee.teams.name}</h3>
+                    <h3>KPIS: {employee.kpis.map(kpi => {
+                        return (
+                            <p>KPI name: {kpi.name} rate: {kpi.pivot.rate} date: {kpi.pivot.KPI_date}</p>
+                        )
+                    })}</h3>
+
+                    <Button onClick={handleClickOpenTeam} >Edit Employee Team</Button>
+                    <Dialog open={openTeam} onClose={handleCloseTeam}>
+                        <DialogTitle> Edit</DialogTitle>
+                        <DialogContent>
+                            <EditEmployeeTeam data={data} />
+                        </DialogContent>
+                    </Dialog>
+
+
+                    <Button onClick={handleClickOpen} >assign KPI</Button>
+                    <Dialog open={open} onClose={handleClose}>
+                        <DialogTitle> Assign a KPI</DialogTitle>
+                        <DialogContent>
+                            <KPIAssignForm id={employee.id} />
+                        </DialogContent>
+                    </Dialog>
+                </div>}
         </>
 
     )
