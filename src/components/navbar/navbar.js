b144d0a32dc31e915f07c15041933c5f8a01ad5e
Malek-Tazkarji <malektazkarjii@gmail.com>
2022-07-21 11:44:04 +0300

    malek change

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,26 @@
+import { TextField } from "@mui/material";
+import React from "react";
+import { useState } from "react";
+ import "./navbar.css";
+export default function NavBar() {
+
+    const admin = localStorage.getItem("admin");
+    const json = JSON.parse(admin);
+    console.log("json", json);
+    const [user, setuser] = useState(json);
+
+   return (
+    <div className="navbarContainer">
+ <img
+                                className="user-image"
+                                src={`http://localhost:8000/storage/uploads/${user.profile_image}`}
+                                alt={user.name}
+                            />
+                            <input class="searchinput" type="text" placeholder="Search"/>
+                            <div className="logotext">
+              <img className="rotate"src={require('./malek.png')}/> 
+              <img className="taz" src={require('./tazkarje.png')}/>
+                </div>
+    </div>
+   );
+}
