fbbb26963e7ad9a1ff9d988884d5362995551305
Ali Haidar <alighaidar97@gmail.com>
2022-08-03 14:50:35 +0300

    ali's stuff merged

--- src/pages/Employee profile/employeeProfile.js
+++ src/pages/Employee profile/employeeProfile.js
@@ -176,7 +176,7 @@ const EmployeeProfile = () => {
                 </DialogContent>
               </Dialog>
             </>
-          )}
+          }
           <Button
             variant="contained"
             className="addEmployeeBtn"
